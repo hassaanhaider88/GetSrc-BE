42f7ad0c57a9f1802a3136bff04bc26ab422a959
hassaan haider <hassaanhaider088@gmail.com>
2025-07-04 06:36:11 +0500

    debugg errors

--- Router/DeleteFileRouter.js
+++ Router/DeleteFileRouter.js
@@ -9,13 +9,13 @@ router.get("/:id", async (req, res) => {
     const deletedFile = await Media.findByIdAndDelete(id);
 
     if (!deletedFile) {
-      return res.status(404).json({ message: "File not found" });
+      return res.status(404).json({ message: "File not found",success:false });
     }
 
-    res.status(202).json({ data: deletedFile });
+    res.status(202).json({ data: deletedFile,success:true });
   } catch (err) {
     console.error("Deletion error:", err);
-    res.status(500).json({ message: "Server error", error: err });
+    res.status(500).json({ message: "Server error", error: err,success:true });
   }
 });
 
