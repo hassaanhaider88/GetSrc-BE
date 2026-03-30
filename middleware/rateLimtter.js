ec8eda8b993361b8b527f4229573301e5b457a81
hassaanhaider88 <hassaanhaider088@gmail.com>
2026-03-30 11:32:25 +0500

    applying express-rate-limit for ascaping Ddos attack

--- middleware/rateLimtter.js
+++ middleware/rateLimtter.js
@@ -1,11 +1,11 @@
 import { rateLimit } from 'express-rate-limit'
 
 const limiter = rateLimit({
-    windowMs: 5 * 60 * 1000,
-    limit: 50,
+    windowMs: 5 * 60 * 1000, 
+    limit: 50, 
     standardHeaders: 'draft-8',
     legacyHeaders: false,
-    ipv6Subnet: 56,
+    ipv6Subnet: 56, 
 })
 
 export default limiter;
