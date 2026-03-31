d532e518057c6687a6b6b043b133a62c4459d482
hassaanhaider88 <hassaanhaider088@gmail.com>
2026-03-31 11:20:00 +0500

    adding realtime pagination with backend integration

--- Router/AllFilesData.js
+++ Router/AllFilesData.js
@@ -2,23 +2,39 @@ import Media from "../Models/MediaMode.js";
 const cache = new Map();
 export async function AllFilesData(req, res) {
   try {
+    const { page } = req.query;
+    if (!page) {
+      return res.status(404).json({ success: false, message: "No files found" });
+    }
+
     const cachedData = await cache.get("files");
     if (cachedData) {
       console.log("serving Cached..")
-      return res.json(cachedData);
+      const sendedRes = cachedData.slice((page - 1) * 40, page * 40);
+      return res.json({
+        success: true,
+        message: "Files fetched successfully",
+        data: sendedRes,
+      });
     }
 
-    const data = await Media.find().sort({ createdAt: -1 });
+    const data = await Media.find().sort();
     if (!data) {
-      return res.status(404).json({ message: "No files found" });
+      return res.status(404).json({ success: false, message: "No files found" });
     }
     cache.set("files", data);
+    const sendedRes = data.slice((page - 1) * 40, page * 40);
 
-    res.json(data);
+    res.json({
+      success: true,
+      message: "Files fetched successfully",
+      data: sendedRes,
+    });
   } catch (error) {
     console.log("Error:", error);
 
     return res.status(500).json({
+      success: false,
       message: "Internal Server Error"
     });
   }
