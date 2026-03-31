afc3c7030b54470513aaa220c4888178dd47d50c
hassaanhaider88 <hassaanhaider088@gmail.com>
2026-03-31 11:37:33 +0500

    working on email

--- Router/emailRouter.js
+++ Router/emailRouter.js
@@ -9,15 +9,15 @@ router.get("/", async (req, res) => {
     if (!email || !email.includes("@")) {
       return res.status(400).json({ error: "Invalid email" });
     }
-    const findEmail = await UserEmail.find({email:email})
-    if(findEmail.length  < 0) return res.status(202).json({ message: "Email Exist",success: false });
+    const findEmail = await UserEmail.find({ email: email })
+    if (findEmail.length > 0) return res.status(202).json({ message: "Email Exist", success: false });
     const newEmail = new UserEmail({ email });
     await newEmail.save();
 
-    res.status(201).json({ message: "Email saved",success: true });
+    res.status(201).json({ message: "Email saved", success: true });
   } catch (error) {
     console.error("Error saving email:", error);
-    res.status(500).json({ error: "Server error",success: false  });
+    res.status(500).json({ error: "Server error", success: false });
   }
 });
 
